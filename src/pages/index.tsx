4b2b727c8f4fc7dea0282939c407d6e991c81a6e
biggEye <96358030+biggeye@users.noreply.github.com>
2023-05-20 12:30:25 -0700

    Update index.tsx
    Signed-off-by: biggEye <96358030+biggeye@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import dynamic from 'next/dynamic';
 import {FC, memo} from 'react';
 
 import Page from '../components/Layout/Page';
-import About from '../components/Sections/About';
 import Contact from '../components/Sections/Contact';
 import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
@@ -22,7 +21,6 @@ const Home: FC = memo(() => {
     <Page description={description} title={title}>
       <Header />
       <Hero />
-      <About />
       <Resume />
       <Portfolio />
       <Testimonials />
